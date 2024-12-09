2bfd3593a9f166496ee3f3cdef2b7cbc17f25d40
Brian MacIntosh <brianamacintosh@gmail.com>
2024-12-09 08:39:32 -0800

    adding additional MIME types

--- WikiCrawler/Utility/MimeUtility.cs
+++ WikiCrawler/Utility/MimeUtility.cs
@@ -14,7 +14,11 @@ public static class MimeUtility
 			case "image/png": return ".png";
 			case "image/bmp": return ".bmp";
 			case "image/tiff": return ".tif";
+			case "image/webp": return ".webp";
 			case "application/pdf": return ".pdf";
+			case "video/mp4": return ".mp4";
+			case "video/mpeg": return ".mpg";
+			case "audio/wav": return ".wav";
 			default:
 				throw new NotImplementedException("No extension for mime '" + mime + "'.");
 		}
@@ -30,10 +34,17 @@ public static class MimeUtility
             case ".gif": return "image/gif";
             case ".jpg": return "image/jpeg";
 			case ".jpeg": return "image/jpeg";
+			case ".jfif": return "image/jpeg";
 			case ".png": return "image/png";
             case ".bmp": return "image/bmp";
 			case ".tif": return "image/tiff";
+			case ".tiff": return "image/tiff";
+			case ".webp": return "image/webp";
 			case ".pdf": return "application/pdf";
+			case ".mp4": return "video/mp4";
+			case ".mpg": return "video/mpeg";
+			case ".mpeg": return "video/mpeg";
+			case ".wav": return "audio/wav";
 			default:
 				throw new NotImplementedException("No mime for extension '" + ext + "'.");
 		}
