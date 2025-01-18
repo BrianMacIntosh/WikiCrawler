0b6f794a244b57c058088c35d0465ceaed8b0e5c
Brian MacIntosh <brianamacintosh@gmail.com>
2025-01-18 13:01:09 -0800

    move heartbeat code to basetask

--- WikiCrawler/Batch/BatchTask.cs
+++ WikiCrawler/Batch/BatchTask.cs
@@ -28,18 +28,10 @@ public abstract class BatchTask : BaseTask, IBatchTask
 
 	public string GetImageCacheDirectory() { return ImageCacheDirectory; }
 
-	public bool HeartbeatEnabled = true;
-
 	/// <summary>
 	/// Should this task persist its progress between runs?
 	/// </summary>
 	protected virtual bool GetPersistStatus() { return true; }
-
-	private Uri m_heartbeatEndpoint;
-	private static readonly TimeSpan HeartbeatInterval = TimeSpan.FromSeconds(60f);
-	private Thread m_heartbeatThread;
-
-	protected Dictionary<string, object> m_heartbeatData = new Dictionary<string, object>();
 
 	protected List<string> m_failMessages = new List<string>();
 
@@ -48,6 +40,8 @@ public abstract class BatchTask : BaseTask, IBatchTask
 
 	public BatchTask(string key)
 	{
+		HeartbeatEnabled = true;
+
 		m_projectKey = key;
 		ProjectDataDirectory = Path.Combine(Configuration.DataDirectory, m_projectKey);
 		ImageCacheDirectory = Path.Combine(ProjectDataDirectory, "images");
@@ -56,66 +50,13 @@ public abstract class BatchTask : BaseTask, IBatchTask
 		m_config = JsonConvert.DeserializeObject<ProjectConfig>(
 			File.ReadAllText(Path.Combine(ProjectDataDirectory, "config.json")));
 
-		m_heartbeatEndpoint = new Uri(File.ReadAllText(Path.Combine(Configuration.DataDirectory, "heartbeat_endpoint.txt")));
-		EasyWeb.SetDelayForDomain(m_heartbeatEndpoint, 0f);
-
 		m_heartbeatData["taskKey"] = m_projectKey;
-		m_heartbeatData["displayName"] = m_config.displayName;
 		m_heartbeatData["nTotal"] = 0;
 		m_heartbeatData["nCompleted"] = 0;
 		m_heartbeatData["nDownloaded"] = 0;
 		m_heartbeatData["nFailed"] = 0;
 		m_heartbeatData["nFailedLicense"] = 0;
 		m_heartbeatData["nDeclined"] = 0;
-		m_heartbeatData["terminate"] = false;
-	}
-
-	protected void StartHeartbeat()
-	{
-		if (HeartbeatEnabled)
-		{
-			m_heartbeatThread = new Thread(HeartbeatThread);
-			m_heartbeatThread.Start();
-		}
-	}
-
-	private void HeartbeatThread()
-	{
-		while (HeartbeatEnabled)
-		{
-			SendHeartbeat(false);
-			Thread.Sleep(HeartbeatInterval);
-		}
-	}
-
-	protected void SendHeartbeat(bool terminate)
-	{
-		if (!HeartbeatEnabled)
-		{
-			return;
-		}
-		if (terminate && m_heartbeatThread != null)
-		{
-			m_heartbeatThread.Abort();
-			m_heartbeatThread = null;
-		}
-		try
-		{
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(m_heartbeatEndpoint);
-			string serialized;
-			lock (m_heartbeatData)
-			{
-				m_heartbeatData["terminate"] = terminate;
-				serialized = JsonConvert.SerializeObject(m_heartbeatData);
-			}
-			string dataString = "d=" + System.Web.HttpUtility.UrlEncode(serialized);
-			Stream response = EasyWeb.Post(request, dataString);
-			response.Dispose();
-		}
-		catch (Exception e)
-		{
-			Console.WriteLine(e.ToString());
-		}
 	}
 
 	protected virtual void SaveOut()
--- WikiCrawler/Tasks/BaseTask.cs
+++ WikiCrawler/Tasks/BaseTask.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Threading;
+using WikiCrawler;
 
 namespace Tasks
 {
@@ -11,9 +13,78 @@ namespace Tasks
 	/// </summary>
 	public abstract class BaseTask
 	{
+		public bool HeartbeatEnabled = false;
+
+		private Uri m_heartbeatEndpoint;
+		private static readonly TimeSpan HeartbeatInterval = TimeSpan.FromSeconds(60f);
+		private Thread m_heartbeatThread;
+
+		protected Dictionary<string, object> m_heartbeatData = new Dictionary<string, object>();
+
+		public BaseTask()
+		{
+			m_heartbeatEndpoint = new Uri(File.ReadAllText(Path.Combine(Configuration.DataDirectory, "heartbeat_endpoint.txt")));
+			EasyWeb.SetDelayForDomain(m_heartbeatEndpoint, 0f);
+
+			m_heartbeatData["taskKey"] = GetType().Name;
+			m_heartbeatData["nEdits"] = 0;
+			m_heartbeatData["terminate"] = false;
+		}
+
 		/// <summary>
 		/// Runs the task synchronously.
 		/// </summary>
 		public abstract void Execute();
+
+		protected void StartHeartbeat()
+		{
+			if (HeartbeatEnabled)
+			{
+				m_heartbeatThread = new Thread(HeartbeatThread);
+				m_heartbeatThread.Start();
+			}
+		}
+
+		private void HeartbeatThread()
+		{
+			while (HeartbeatEnabled)
+			{
+				SendHeartbeat(false);
+				Thread.Sleep(HeartbeatInterval);
+			}
+		}
+
+		protected void SendHeartbeat(bool terminate)
+		{
+			if (!HeartbeatEnabled)
+			{
+				return;
+			}
+			if (terminate && m_heartbeatThread != null)
+			{
+				m_heartbeatThread.Abort();
+				m_heartbeatThread = null;
+			}
+			try
+			{
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(m_heartbeatEndpoint);
+				string serialized;
+				lock (m_heartbeatData)
+				{
+					m_heartbeatData["terminate"] = terminate;
+					serialized = JsonConvert.SerializeObject(m_heartbeatData);
+				}
+				string dataString = "d=" + System.Web.HttpUtility.UrlEncode(serialized);
+				Stream response = EasyWeb.Post(request, dataString);
+				response.Dispose();
+
+				// edits are additive
+				m_heartbeatData["nEdits"] = 0;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
 	}
 }
