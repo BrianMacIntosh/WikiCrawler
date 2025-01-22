3ed3e2fff9c7363f3c7f1bd10eca4e0ff7e2b77d
Brian MacIntosh <brianamacintosh@gmail.com>
2025-01-21 23:56:38 -0800

    added DownloadCategoryList task

---
+++ WikiCrawler/Tasks/DownloadCategoryList.cs
@@ -0,0 +1,25 @@
+﻿using MediaWiki;
+using System;
+using System.IO;
+using WikiCrawler;
+
+namespace Tasks
+{
+	public class DownloadCategoryList : BaseTask
+	{
+		public override void Execute()
+		{
+			using (StreamWriter writer = new StreamWriter(Path.Combine(Configuration.DataDirectory, "primary-license-tags.txt")))
+			{
+				foreach (Article page in GlobalAPIs.Commons.GetCategoryEntries("Category:Primary license tags (flat list)", CMType.page))
+				{
+					if (page.title.StartsWith("Template:"))
+					{
+						Console.WriteLine(page.title);
+						writer.WriteLine(page.title.Substring("Template:".Length));
+					}
+				}
+			}
+		}
+	}
+}
