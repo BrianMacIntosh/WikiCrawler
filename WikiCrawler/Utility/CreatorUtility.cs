47a893064a0caa866e6d96544f6bfb26bac3a66b
Brian MacIntosh <brianamacintosh@gmail.com>
2025-02-26 23:10:26 -0800

    fix creator regex matching across multiple creators

--- WikiCrawler/Utility/CreatorUtility.cs
+++ WikiCrawler/Utility/CreatorUtility.cs
@@ -227,7 +227,7 @@ namespace MediaWiki
 			}
 		}
 
-		private static readonly Regex CreatorTemplateRegex = new Regex(@"^{{\s*([Cc]reator:.+)}}$");
+		private static readonly Regex CreatorTemplateRegex = new Regex(@"^{{\s*([Cc]reator:[^{}]+)}}$");
 
 		public static readonly Regex InlineCreatorTemplateRegex = new Regex(@"^{{\s*[Cc]reator\s*\|\s*[Ww]ikidata\s*=\s*(Q[0-9]+)\s*}}$");
 		public static readonly Regex AuthorLifespanRegex = new Regex(@"^([^\(\)]+)\s+\(?([0-9][0-9][0-9][0-9])[\-–—]([0-9][0-9][0-9][0-9])\)?$");
