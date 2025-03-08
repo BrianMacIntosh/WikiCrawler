f906bd0d92388699542acab92ecffff23dc9ea71
Brian MacIntosh <brianamacintosh@gmail.com>
2025-03-07 23:01:03 -0800

    added a task to refresh the pdart cache

---
+++ WikiCrawler/Tasks/Commons/PdArtRefreshCache.cs
@@ -0,0 +1,24 @@
+﻿using MediaWiki;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace Tasks.Commons
+{
+	public class PdArtRefreshCache : ReplaceInCategory
+	{
+		public PdArtRefreshCache()
+			: base(new PdArtReplacement())
+		{
+			PdArtReplacement.SkipCached = false;
+			ImplicitCreatorsReplacement.SlowCategoryWalk = false;
+			PdArtReplacement.SkipAuthorLookup = true;
+		}
+
+		public override string GetCategory()
+		{
+			return "Category:PD-Art (PD-old default)";
+			//return "Category:PD-Art (PD-old-70)";
+		}
+	}
+}
