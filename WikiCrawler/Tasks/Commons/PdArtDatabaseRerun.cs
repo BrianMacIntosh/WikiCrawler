112096fbb406f0f7795b6d78a75df6c8b1bf52b5
Brian MacIntosh <brianamacintosh@gmail.com>
2025-03-16 14:51:02 -0700

    added PdArtDatabaseRerun for rerunning on files from the pdart database with a query

---
+++ WikiCrawler/Tasks/Commons/PdArtDatabaseRerun.cs
@@ -0,0 +1,44 @@
+﻿using MediaWiki;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace Tasks.Commons
+{
+	public class PdArtDatabaseRerun : ReplaceIn
+	{
+		public PdArtDatabaseRerun()
+			: base(new PdArtReplacement())
+		{
+			PdArtReplacement.SkipCached = false;
+			ImplicitCreatorsReplacement.SlowCategoryWalk = false; // already tried last time
+			PdArtReplacement.SkipAuthorLookup = true; // already tried last time
+
+			HeartbeatEnabled = true;
+			m_heartbeatData["taskKey"] = "PdArtFollowup";
+		}
+
+		public override IEnumerable<Article> GetPagesToAffectUncached(string startSortkey)
+		{
+			return GlobalAPIs.Commons.FetchArticles(GetDummyArticles());
+		}
+
+		private IEnumerable<Article> GetDummyArticles()
+		{
+			SQLiteConnection connection = PdArtReplacement.ConnectFilesDatabase(false);
+			SQLiteCommand query = connection.CreateCommand();
+			query.CommandText = "SELECT pageTitle FROM files WHERE bLicenseReplaced=2";
+
+			List<Article> results = new List<Article>();
+			using (SQLiteDataReader reader = query.ExecuteReader())
+			{
+				while (reader.Read())
+				{
+					results.Add(new Article(reader.GetString(0)));
+				}
+			}
+
+			connection.Close();
+			return results;
+		}
+	}
+}
