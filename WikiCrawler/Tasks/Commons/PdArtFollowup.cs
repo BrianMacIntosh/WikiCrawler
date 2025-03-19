14a0c6409bf9c02a171088b0071c5d46bc7a6a58
Brian MacIntosh <brianamacintosh@gmail.com>
2025-03-18 21:03:42 -0700

    PdArtFollowup: creator fixes before date

--- WikiCrawler/Tasks/Commons/PdArtFollowup.cs
+++ WikiCrawler/Tasks/Commons/PdArtFollowup.cs
@@ -1,6 +1,7 @@
 ﻿using MediaWiki;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using WikiCrawler;
 
 namespace Tasks
@@ -16,29 +17,31 @@ namespace Tasks
 		public override IEnumerable<Article> GetPagesToAffectUncached(string startSortkey)
 		{
 			PdArtReplacement.SkipCached = false;
-			ImplicitCreatorsReplacement.SlowCategoryWalk = false;
+			ImplicitCreatorsReplacement.SlowCategoryWalk = false; // already tried last time
+			PdArtReplacement.SkipAuthorLookup = true; // already tried last time
 
+			string pdArtDirectory = Path.Combine(Configuration.DataDirectory, "PdArtReplacement");
+			ManualMapping<MappingCreator> creatorMapping = new ManualMapping<MappingCreator>(ImplicitCreatorsReplacement.GetCreatorMappingFile(pdArtDirectory));
 			ManualMapping<MappingDate> dateMapping = new ManualMapping<MappingDate>(PdArtReplacement.DateMappingFile);
-			foreach (var kv in dateMapping)
+
+			foreach (var kv in creatorMapping)
 			{
-				if (!string.IsNullOrEmpty(kv.Value.ReplaceDate)
-					|| kv.Value.LatestYear != 9999
-					|| PdArtReplacement.ParseDate(kv.Key).LatestYear != 9999)
+				if (!string.IsNullOrEmpty(kv.Value.MappedValue)
+					|| !string.IsNullOrEmpty(kv.Value.MappedQID))
 				{
-					Article[] articles = GlobalAPIs.Commons.GetPages(kv.Value.FromPages);
-					foreach (Article article in articles)
+					//TODO: GetPages should automatically break up file lists
+					foreach (Article article in GlobalAPIs.Commons.FetchArticles(kv.Value.FromPages.Select(title => new Article(title))))
 					{
 						yield return article;
 					}
 				}
 			}
 
-			string pdArtDirectory = Path.Combine(Configuration.DataDirectory, "PdArtFixup");
-			ManualMapping<MappingCreator> creatorMapping = new ManualMapping<MappingCreator>(ImplicitCreatorsReplacement.GetCreatorMappingFile(pdArtDirectory));
-			foreach (var kv in creatorMapping)
+			foreach (var kv in dateMapping)
 			{
-				if (!string.IsNullOrEmpty(kv.Value.MappedValue)
-					|| !string.IsNullOrEmpty(kv.Value.MappedQID))
+				if (!string.IsNullOrEmpty(kv.Value.ReplaceDate)
+					|| kv.Value.LatestYear != 9999
+					|| PdArtReplacement.ParseDate(kv.Key).LatestYear != 9999)
 				{
 					Article[] articles = GlobalAPIs.Commons.GetPages(kv.Value.FromPages);
 					foreach (Article article in articles)
