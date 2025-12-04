ef512f965e11c6a8ff127461afccd0eb12d8259c
Brian MacIntosh <brianamacintosh@gmail.com>
2025-12-04 08:51:07 -0800

    Create QIdTests.cs

---
+++ WikiCrawlerTests/QIdTests.cs
@@ -0,0 +1,13 @@
+﻿using MediaWiki;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class QIdTests
+{
+	[TestMethod]
+	public void Parse()
+	{
+		Assert.AreEqual(new QId(1234), QId.Parse("Q1234"));
+		Assert.AreEqual(new QId(1234), QId.Parse("1234"));
+	}
+}
