589f0f0eb481afe7a9043fe78d65bb03cf6a374a
Brian MacIntosh <brianamacintosh@gmail.com>
2025-12-22 12:05:07 -0800

    SayreCategorize handles more role prefixes and maps categories

--- WikiCrawler/UWash/SayreCategorize.cs
+++ WikiCrawler/UWash/SayreCategorize.cs
@@ -2,15 +2,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using WikiCrawler;
 
 namespace UWash
 {
 	public static class SayreCategorize
 	{
-		public static void ProduceSayreCategories(Api commonsApi,
-			CategoryTree catTree,
+		private static string[] RolePrefixes = new string[]
+		{
+			"Actor ",
+			"Actress ",
+			"Singer ",
+			"Opera singer ",
+			"Radio entertainer ",
+			"Entertainer ",
+			"Stage actor ",
+			"Stage actress ",
+			"Stage singer ",
+			"Stage director ",
+			"Stage comedian ",
+			"Musical comedian ",
+			"Dancer ",
+			"Film actor ",
+			"Film actoress ",
+			"Film juvenile ",
+			"Film comedian ",
+			"Screen actor ",
+			"Screen actoress ",
+			"Radio comedian ",
+			"Concert pianist ",
+			"Silent film actor ",
+			"Silent film actress ",
+			"Radio xylophone soloist ",
+		};
+
+		public static void ProduceSayreCategories(Api commonsApi, CategoryTree catTree,
+			int key, CategoryMapping categoryMappings,
 			Dictionary<string, string> metadata, HashSet<PageTitle> categories)
 		{
+			TaskItemKeyString keyString = new TaskItemKeyString(key.ToString());
+
 			//TODO: only do this for sayre uploads?
 
 			string title = metadata["Title"];
@@ -35,9 +66,9 @@ namespace UWash
 				{
 					// check that it's a play or film
 					catTree.AddToTree(productionCategory.title, 6);
-					if (catTree.HasParent(productionCategory.title, "Category:Films")
+					/*if (catTree.HasParent(productionCategory.title, "Category:Films")
 						|| catTree.HasParent(productionCategory.title, "Category:Plays")
-						|| catTree.HasParent(productionCategory.title, "Category:Musicals"))
+						|| catTree.HasParent(productionCategory.title, "Category:Musicals"))*/
 					{
 						// it is!
 						categories.Add(productionCategory.title);
@@ -48,7 +79,7 @@ namespace UWash
 			// check if it's a photo of a person
 			List<string> maybeNames = new List<string>();
 			
-			// pull out any names from the title
+			// pull out any possible names from the title
 			string[] commaSplit = title.Split(new char[] { ',' }, 2);
 			if (commaSplit.Length == 2)
 			{
@@ -69,7 +100,7 @@ namespace UWash
 			{
 				maybeNames.AddRange(includesSplit[1].Split(new string[] { " and " }, StringSplitOptions.RemoveEmptyEntries));
 			}
-			if (title.StartsWith("Actor ") || title.StartsWith("Actress "))
+			if (RolePrefixes.Any(prefix => title.StartsWith(prefix, StringComparison.OrdinalIgnoreCase)))
 			{
 				maybeNames.Add(title);
 			}
@@ -81,16 +112,37 @@ namespace UWash
 
 			foreach (string name in maybeNames)
 			{
-				string nametrim = name.Trim().TrimStart("Actress ").TrimStart("Actor ");
+				// check already mapped
+				MappingCategory mapping = categoryMappings.TryGetValue(name);
+				if (mapping != null)
+				{
+					if (!mapping.IsUnmapped)
+					{
+						categories.Add(PageTitle.Parse(mapping.MappedCategory));
+					}
+				}
+
+				// try mapping
+				mapping = categoryMappings.TryMapValue(name, keyString);
+
+				string nametrim = name.Trim();
+				foreach (string role in RolePrefixes)
+				{
+					if (nametrim.StartsWith(role, StringComparison.OrdinalIgnoreCase))
+					{
+						nametrim = nametrim.Substring(role.Length);
+					}
+				}
+
 				Article personCategory = commonsApi.GetPage(new PageTitle(PageTitle.NS_Category, nametrim));
 				if (personCategory != null && !personCategory.missing)
 				{
 					// check that it's an actor/ess
 					catTree.AddToTree(personCategory.title, 6);
-					if (catTree.HasParent(personCategory.title, "Category:Performing artists"))
+					//if (catTree.HasParent(personCategory.title, "Category:Performing artists"))
 					{
 						//TODO: check lifetime?
-
+						mapping.MappedCategory = personCategory.title.ToString();
 						categories.Add(personCategory.title);
 					}
 				}
