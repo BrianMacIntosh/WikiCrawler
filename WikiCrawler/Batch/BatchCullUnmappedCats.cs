83512acc5e42794ecc529db97a8b84b3fd5f7e7e
Brian MacIntosh <brianamacintosh@gmail.com>
2025-12-22 12:03:44 -0800

    Create BatchCullUnmappedCats.cs

---
+++ WikiCrawler/Batch/BatchCullUnmappedCats.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WikiCrawler;
+
+namespace Tasks
+{
+	/// <summary>
+	/// Task that removes unmapped entries from a specified project's category mappings.
+	/// </summary>
+	public class BatchCullUnmappedCats : BaseTask
+	{
+		public override void Execute()
+		{
+			Console.Write("Project Key>");
+			string projectKey = Console.ReadLine();
+			string projectDir = Path.Combine(Configuration.DataDirectory, projectKey);
+			CategoryMapping mapping = new CategoryMapping(Path.Combine(projectDir, "categories.json"));
+			List<string> killList = new List<string>();
+			foreach (string key in mapping.Keys)
+			{
+				MappingCategory cat = mapping.TryGetValue(key);
+				if (cat.IsUnmapped)
+				{
+					killList.Add(key);
+				}
+			}
+			Console.WriteLine("Culling {0} keys.", killList.Count);
+			foreach (string key in killList)
+			{
+				mapping.Remove(key);
+			}
+			mapping.Serialize();
+		}
+	}
+}
