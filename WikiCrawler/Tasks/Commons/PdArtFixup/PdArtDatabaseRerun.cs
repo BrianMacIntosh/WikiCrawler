3305d80ae4ade98dc97245547f4f0ba5af7b3e10
Brian MacIntosh <brianamacintosh@gmail.com>
2025-12-23 14:20:38 -0800

    Update PdArtDatabaseRerun.cs

--- WikiCrawler/Tasks/Commons/PdArtFixup/PdArtDatabaseRerun.cs
+++ WikiCrawler/Tasks/Commons/PdArtFixup/PdArtDatabaseRerun.cs
@@ -11,17 +11,18 @@ namespace Tasks.Commons
 			: base(
 				  new ImplicitCreatorsReplacement("FixImplicitCreators"),
 				  new LocalizeDateReplacement(),
-				  new PdArtReplacement())
+				  new PdArtReplacement(),
+				  //new LicenseHeaderCleanup(),
+				  )
 		{
 			ImplicitCreatorsReplacement.SkipCached = false;
 			PdArtReplacement.SkipCached = false;
 			ImplicitCreatorsReplacement.SlowCategoryWalk = false; // already tried last time
 
 			//TODO: SELECT * from files WHERE irreplaceableLicenses LIKE "PD-US-unpublished" AND replaced!=1
-			//Parameters["Query"] = "SELECT pageTitle from files WHERE irreplaceableLicenses LIKE \"PD-US-unpublished\" AND replaced!=1";
 
-			Parameters["Query"] = "SELECT * FROM files WHERE replaced=0 AND latestYear=9999 AND authorDeathYear<2025-95-120";
-
+			Parameters["Query"] = "SELECT * FROM files WHERE replaced=0 AND latestYear=9999 AND authorDeathYear<2025-95-120 AND touchTimeUnix<1749831847";
+			//Parameters["Query"] = "SELECT 'File:Gerard ter Borch Westfälischer Friede Kopie Münster.jpg'";
 			//Parameters["Query"] = "SELECT pageTitle FROM files where pdArtLicense LIKE \"{{pd-art}}\" AND replaced=0";
 		}
 
